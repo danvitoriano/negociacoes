38f2c6c52ed264ce21881a0d488e50042fa5acaa
Logon PF Fiap <logonpflocal@fiap.com.br>
2020-02-13 18:47:06 -0300

    apaga

---
+++ client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,21 @@
+class ListaNegociacoes {
+    
+    constructor() {
+        
+        this._negociacoes = [];
+    }
+    
+    adiciona(negociacao) {
+        
+        this._negociacoes.push(negociacao);
+    }
+
+    esvazia() {
+        this._negociacoes = [];
+    }
+    
+    get negociacoes() {
+        
+        return [].concat(this._negociacoes);
+    }
+}
