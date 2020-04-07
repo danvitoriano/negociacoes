6fe0ae5f2296daeb410504f00837d062d2a8676d
eduardosukeda <eduardosukeda@live.com>
2020-04-07 00:33:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http')
     ,app = require('./config/express');
 
-http.createServer(app).listen(function() {
+http.createServer(app).listen(process.env.PORT, function() {
     console.log('Servidor estutando na porta: ' + this.address().port);
 });
 
