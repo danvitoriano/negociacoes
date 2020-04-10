c3ef9b416405155abd078a0bc302cad56cbac121
Lucas Duarte <l.duarte.mk@gmail.com>
2020-04-10 19:51:26 -0300

    added start DAO Class

---
+++ client/js/app/dao/NegociacoesDao.js
@@ -0,0 +1,11 @@
+class NegociacaoDao {
+  constructor(connection) {
+    // resolve a conexão com o banco
+  }
+  adiciona(negociacao) {
+    return new Promise((resolve, reject) => {
+      // resolve o request no banco em caso de
+      // sucesso ou erro
+    });
+  }
+}
