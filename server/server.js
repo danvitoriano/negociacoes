19b9fb5ced5e464ef428fa3bc0a5e6b1ff50821c
Vinicius <als.vinicius@gmail.com>
2020-04-12 15:52:53 -0300

    Deploy no firebase

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var http = require('http')
     ,app = require('./config/express');
+const functions = require("firebase-functions");
 
 http.createServer(app).listen(3000, function() {
     console.log('Servidor estutando na porta: ' + this.address().port);
 });
-
