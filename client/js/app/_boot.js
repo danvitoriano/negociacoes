4e6a569bae415187104cc200b26afd9cffcb797f
guiarese <guilhermearesende1@gmail.com>
2020-04-12 01:32:23 -0300

    Inclusão de Filtros

--- client/js/app/_boot.js
+++ client/js/app/_boot.js
@@ -1,6 +1,7 @@
-import { NegociacaoController } from './controllers/NegociacaoController';
-let negociacaoController = new NegociacaoController();
+import {currentInstance} from './controllers/NegociacaoController';
+
+let negociacaoController = currentInstance();
 
 document.querySelector('.form').onsubmit = negociacaoController.adiciona.bind(negociacaoController);
-document.querySelector('#apaga-negociacoes-btn').onclick = negociacaoController.apaga.bind(negociacaoController);
 document.querySelector('#importa-negociacoes-btn').onclick = negociacaoController.importaNegociacoes.bind(negociacaoController);
+document.querySelector('#apaga-negociacoes-btn').onclick = negociacaoController.apaga.bind(negociacaoController);
--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,11 +1,19 @@
-import { View } from './View';
-import { DateHelper } from '../helpers/DateHelper';
-// import { currentInstance } from '../controllers/NegociacaoController';
+import {View} from './View';
+import {DateHelper} from '../helpers/DateHelper';
+import {currentInstance} from '../controllers/NegociacaoController';
 
 export class NegociacoesView extends View {
 
     constructor(elemento) {
+
         super(elemento);
+        
+        elemento.addEventListener('click', function(event) {
+
+            if(event.target.nodeName == 'TH')
+                currentInstance().ordena(event.target.textContent.toLowerCase());    
+        });
+        
     }
 
     template(model) {
