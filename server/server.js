a6ea1afab48c42b2d8952109dfdc53d85caaabf3
Thiago Veiga <thiago.veiga@totvs.com.br>
2020-04-13 21:28:33 -0300

    Persistir Negociações no MongoDB #19 create connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 var http = require('http')
-    ,app = require('./config/express');
+    ,app = require('./config/express')
+    ,mongodb = require("mongodb")
+    ,db = null;
 
+mongodb.MongoClient.connect("mongodb+srv://admin:admin@cluster0-ichd6.mongodb.net/test?retryWrites=true&w=majority", function (err, client) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+      
+    db = client.db();
+    console.log("MongoDb conectado");
+      
+});
+
+      
 http.createServer(app).listen(3000, function() {
     console.log('Servidor estutando na porta: ' + this.address().port);
 });
+
 
