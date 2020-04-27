0d9bf1bc9bbe084bfd4fe52e3c3e5f55a9806adb
Thiago Veiga <thiago.veiga@totvs.com.br>
2020-04-26 21:41:35 -0300

    crud methods

--- server/app/database/index.js
+++ server/app/database/index.js
@@ -16,13 +16,65 @@ database.find = function(colName, negociacao){
         var con = client.db();
              
         result = con.collection(colName).find(negociacao);
-        console.log('1 ' + result);
         client.close();
     });
 
-    console.log('2 ' + result);
     return result;
 }
 
+database.insert = function(colName, negociacao){
+    var result = {};
+
+    client.connect(url, function (err, client) {
+    
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        var con = client.db();
+             
+        result = con.collection(colName).insert(negociacao);
+        client.close();
+    });
+
+    return result;
+}
+
+database.update = function(colName, negociacaoOld, negociacaoNew){
+    var result = {};
+
+    client.connect(url, function (err, client) {
+    
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        var con = client.db();
+             
+        result = con.collection(colName).update(negociacaoOld, negociacaoNew)
+        client.close();
+    });
+
+    return result;
+}
+
+
+database.update = function(colName, negociacao){
+    var result = {};
+
+    client.connect(url, function (err, client) {
+    
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        var con = client.db();
+             
+        result = con.collection(colName).remove(negociacao)
+        client.close();
+    });
+
+    return result;
+}
 
 module.exports = database;
