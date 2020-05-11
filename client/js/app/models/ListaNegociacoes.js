e9b096a3f6efeac427d88ca8c69f37ad0f4f3103
Your Name <you@example.com>
2020-05-11 12:43:44 -0300

    Filtro por Data - Issue11.

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -18,4 +18,21 @@ export class ListaNegociacoes {
 
         return [].concat(this._negociacoes);
     }
+
+    filtraData(dataRecebida) {
+        let negociacaoFilter = this._negociacoes.filter(function (item){
+            let data1 = new Date(item._data);
+            let data2 = new Date(dataRecebida);
+
+            if(
+                data1.getDate() == (data2.getDate() + 1)
+                && (data1.getMonth() + 1) == (data2.getMonth() + 1)
+                && data1.getFullYear() == data2.getFullYear()
+            ){
+                return item;
+            }
+        })
+
+        this._negociacoes = negociacaoFilter;
+    }
 }
