f6555e42212ebaf09dfbff69e66f119ef5c199b7
Danilo Vitoriano <vitoriano08@gmail.com>
2020-08-29 01:54:24 -0300

    update packages

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,18 +1,16 @@
-/* Código simplório, apenas para fornecer o serviço para a aplicação */
-
 var api = require('../api');
 
-module.exports  = function(app) {
-    
+module.exports = function (app) {
+
     app.route('/negociacoes/semana')
         .get(api.listaSemana);
-        
+
     app.route('/negociacoes/anterior')
         .get(api.listaAnterior);
-        
+
     app.route('/negociacoes/retrasada')
-        .get(api.listaRetrasada);  
-        
+        .get(api.listaRetrasada);
+
     app.route('/negociacoes')
-        .post(api.cadastraNegociacao);          
+        .post(api.cadastraNegociacao);
 };
